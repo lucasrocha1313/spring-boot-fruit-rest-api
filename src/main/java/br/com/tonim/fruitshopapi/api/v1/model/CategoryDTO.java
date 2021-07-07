f9fc75b0f98e36e06247deed298ba26438f466dd
tonim <lucas.rocha@dtidigital.com.br>
2021-07-07 11:01:03 -0300

    Category creation

---
+++ src/main/java/br/com/tonim/fruitshopapi/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package br.com.tonim.fruitshopapi.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/br/com/tonim/fruitshopapi/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package br.com.tonim.fruitshopapi.repositories;
+
+import br.com.tonim.fruitshopapi.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
