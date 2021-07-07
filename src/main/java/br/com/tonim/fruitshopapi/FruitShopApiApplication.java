30b3c2f0d9d1bbf83d641da74a2ddedd0d612575
tonim <lucas.rocha@dtidigital.com.br>
2021-07-07 10:48:56 -0300

    init project files

---
+++ src/main/java/br/com/tonim/fruitshopapi/FruitShopApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tonim.fruitshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tonim/fruitshopapi/FruitShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tonim.fruitshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
